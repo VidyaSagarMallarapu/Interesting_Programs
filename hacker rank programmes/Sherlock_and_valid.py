0666f059ad874cd254b99d8531e10c15df297ad6
=VidyaSagarMallarapu <mallarapuvidyasagar@gmail.com>
2019-02-06 21:38:11 +0530

    Update Sherlock_and_valid.py

--- hacker rank programmes/Sherlock_and_valid.py
+++ hacker rank programmes/Sherlock_and_valid.py
@@ -21,7 +21,7 @@ else :print("NO")
 
 
 """
-qestion 
+question 
 
 Sherlock considers a string to be valid if all characters of the string appear the same number of times. It is also valid if he can remove just character at index in the string, and the remaining characters will occur the same number of times. Given a string
 
